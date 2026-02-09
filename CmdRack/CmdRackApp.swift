46db7641c5bc38513837b2f2421e7b5d99975759
Dheeraj rao <ydheerajrao8857@gmail.com>
2026-02-09 07:54:02 +0530

    Initial Commit

---
+++ CmdRack/CmdRackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CmdRackApp.swift
+//  CmdRack
+//
+//  Created by Dheeraj Rao on 09/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CmdRackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CmdRackTests/CmdRackTests.swift
@@ -0,0 +1,17 @@
+//
+//  CmdRackTests.swift
+//  CmdRackTests
+//
+//  Created by Dheeraj Rao on 09/02/26.
+//
+
+import Testing
+@testable import CmdRack
+
+struct CmdRackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
